6ff57f9b389d4e190193922cf8c628a7d9c83578
suyash-5613 <suyashsahu0405@gmail.com>
2025-09-11 12:30:00 +0530

    ding dong

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/ecommerce/',  // 👈 repo
-})
+  base: command === 'build' ? '/ecommerce-frontend/' : '/ecommerce/',
+}))
